2fe380bf476ea8fd5fc0fcdbdf3c0d0e875c7898
wangshouan <B11415007@mail.ntust.edu.tw>
2026-01-20 16:17:55 +0800

    a replace button

--- src/App.vue
+++ src/App.vue
@@ -186,77 +186,77 @@ const handleMouseLeave = () => {
     <section>
       <common-function-container>
         <common-function-block>
-          <button>
+          <a href="https://www.google.com/">
             <img
               src="https://my.tcb-life.com.tw/api/assets/ae2ec3b4-e3ec-4b5e-9d5b-55fe97a45fc5"
               alt="網路投保"
               draggable="false"
             />
             <p>網路投保</p>
-          </button>
+          </a>
           <divider />
-          <button>
+          <a>
             <img
               src="https://my.tcb-life.com.tw/api/assets/df0db06d-44f8-42e5-8cc7-8a9baf2108fa"
               alt="電銷商品"
               draggable="false"
             />
             <p>電銷商品</p>
-          </button>
+          </a>
           <divider />
-          <button>
+          <a>
             <img
               src="https://my.tcb-life.com.tw/api/assets/5c9b3d21-bcad-4ca4-be9f-c0e593eca949"
               alt="保單查詢"
               draggable="false"
             />
             <p>保單查詢</p>
-          </button>
+          </a>
           <divider />
-          <button>
+          <a>
             <img
               src="https://my.tcb-life.com.tw/api/assets/aa6871c4-3e20-4071-b6e0-f95b0eab2aaf"
               alt="契約變更"
               draggable="false"
             />
             <p>契約變更</p>
-          </button>
+          </a>
           <divider />
-          <button>
+          <a>
             <img
               src="https://my.tcb-life.com.tw/api/assets/8a411768-6276-489d-8659-46233472f6e7"
               alt="保單理賠"
               draggable="false"
             />
             <p>保單理賠</p>
-          </button>
+          </a>
           <divider />
-          <button>
+          <a>
             <img
               src="https://my.tcb-life.com.tw/api/assets/d90cd0cc-929d-40c0-be7a-34fa69447037"
               alt="利率看板"
               draggable="false"
             />
             <p>利率看板</p>
-          </button>
+          </a>
           <divider />
-          <button>
+          <a>
             <img
               src="https://my.tcb-life.com.tw/api/assets/b45e7d96-4201-47af-b680-5fdfbd2b6e67"
               alt="標的專區"
               draggable="false"
             />
             <p>標的專區</p>
-          </button>
+          </a>
           <divider />
-          <button>
+          <a>
             <img
               src="https://my.tcb-life.com.tw/api/assets/bc967f6b-9084-4229-8fba-49303bd7c0d1"
               alt="表單下載"
               draggable="false"
             />
             <p>表單下載</p>
-          </button>
+          </a>
         </common-function-block>
         <search-input-container>
           <input type="search" placeholder="我還想查詢.." />
@@ -471,6 +471,7 @@ swiper-slide {
   }
   @media screen and (max-width: 840px) {
     width: 90%;
+    left: 0;
     transform: translateX(5%);
   }
 }
@@ -498,7 +499,7 @@ common-function-block {
   width: 60%;
   border-radius: 10px;
 
-  button {
+  a {
     all: unset;
     cursor: pointer;
     box-sizing: border-box;
