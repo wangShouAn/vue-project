fea3608b8b062382a648679e225018d8db735a10
wangshouan <B11415007@mail.ntust.edu.tw>
2026-01-21 10:17:15 +0800

    PopularEvents

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import CommonFunctionContainer from './components/CommonFunctionContainer.vue'
 import SearchInputContainer from './components/SearchInputContainer.vue'
 import InteractiveSituation from './components/InteractiveSituation.vue'
 import ProductAreaContainer from './components/ProductAreaContainer.vue'
+import PopularEvents from './components/PopularEvents.vue'
 
 const isNavSticky = ref(false)
 const Sticky = ref(null)
@@ -124,21 +125,7 @@ const handleMouseLeave = () => {
     </section>
     <InteractiveSituation />
     <section><ProductAreaContainer /></section>
-    <section>
-      <popular-events-header>
-        <h1>熱門活動</h1>
-      </popular-events-header>
-      <a id="gf" href="#" style="margin: 5vw 12.5%; width: 75%; position: relative">
-        <img
-          src="https://my.tcb-life.com.tw/api/assets/b3b9cf03-081c-452a-880c-7596c41b0821"
-          style="width: 75%"
-        />
-        <img
-          id="gfr"
-          src="https://my.tcb-life.com.tw/api/assets/e089121e-078b-41d1-93b4-0b38c41b9f77"
-        />
-      </a>
-    </section>
+    <section><PopularEvents /></section>
     <section>
       <video-area-block>
         <iframe
@@ -268,36 +255,6 @@ nav {
   }
 }
 
-popular-events-header {
-  position: relative;
-  left: 20%;
-  transform: translateX(-50%);
-  transform: translateY(-3vw);
-  h1 {
-    color: #0e8e6c;
-    font-size: 1.5vw;
-  }
-  @media screen and (max-width: 840px) {
-    left: 10%;
-    h1 {
-      font-size: 2.5vw;
-    }
-  }
-}
-#gf {
-  #gfr {
-    display: none;
-  }
-  @media screen and (max-width: 840px) {
-    img {
-      display: none;
-    }
-    #gfr {
-      display: block;
-      width: 100%;
-    }
-  }
-}
 video-area-block {
   display: flex;
   justify-content: center;
---
+++ src/components/PopularEvents.vue
@@ -0,0 +1,47 @@
+<template>
+  <popular-events-header>
+    <h1>熱門活動</h1>
+  </popular-events-header>
+  <a id="gf" href="#" style="margin: 5vw 12.5%; width: 75%; position: relative">
+    <img
+      src="https://my.tcb-life.com.tw/api/assets/b3b9cf03-081c-452a-880c-7596c41b0821"
+      style="width: 75%"
+    />
+    <img
+      id="gfr"
+      src="https://my.tcb-life.com.tw/api/assets/e089121e-078b-41d1-93b4-0b38c41b9f77"
+    />
+  </a>
+</template>
+<style lang="scss" scoped>
+popular-events-header {
+  position: relative;
+  left: 20%;
+  transform: translateX(-50%);
+  transform: translateY(-3vw);
+  h1 {
+    color: #0e8e6c;
+    font-size: 1.5vw;
+  }
+  @media screen and (max-width: 840px) {
+    left: 10%;
+    h1 {
+      font-size: 2.5vw;
+    }
+  }
+}
+#gf {
+  #gfr {
+    display: none;
+  }
+  @media screen and (max-width: 840px) {
+    img {
+      display: none;
+    }
+    #gfr {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+</style>
